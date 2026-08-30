39d219a2276757333c7cef865d7eb5080b24f50e
Juno Nguyen <juno.ngx@gmail.com>
2026-08-30 12:20:42 +0800

    feat: update refresh token max age to one year

--- api/src/lib/constants.ts
+++ api/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const REFRESH_TOKEN_MAX_AGE = 7 * 24 * 60 * 60
+export const REFRESH_TOKEN_MAX_AGE = 7 * 24 * 60 * 365
 
 export const RATE_LIMIT_AUTH = { limit: 10, windowMs: 60_000 }
 export const RATE_LIMIT_LINK = { limit: 30, windowMs: 60_000 }
